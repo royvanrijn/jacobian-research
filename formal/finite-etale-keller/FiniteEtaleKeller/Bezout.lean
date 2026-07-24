2226c35a5082db4e38bea9d33d67647b7ba11eb2
Roy van Rijn <roy.van.rijn@gmail.com>
2026-07-24 22:32:24 +0200

    Add formal source provenance header

--- formal/finite-etale-keller/FiniteEtaleKeller/Bezout.lean
+++ formal/finite-etale-keller/FiniteEtaleKeller/Bezout.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Roy van Rijn. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Roy van Rijn
+-/
 import Mathlib.RingTheory.AdjoinRoot
 
 /-!
